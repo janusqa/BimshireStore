a7721aa0acad307d5a3f51d9ce98cdc619a204e8
Janus Developer <dev@retrievo.net>
2024-06-05 11:45:06 -0400

    add migrations for ProductAPI due to model change

--- BimshireStore.Services.ProductAPI/Migrations/20240605154410_AddLocalImagePathFieldToProductModelInProductAPIDb.Designer.cs
+++ BimshireStore.Services.ProductAPI/Migrations/20240605154410_AddLocalImagePathFieldToProductModelInProductAPIDb.Designer.cs
@@ -2,6 +2,7 @@
 using BimshireStore.Services.ProductAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BimshireStore.Services.ProductAPI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240605154410_AddLocalImagePathFieldToProductModelInProductAPIDb")]
+    partial class AddLocalImagePathFieldToProductModelInProductAPIDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.5");
---
+++ BimshireStore.Services.ProductAPI/Migrations/20240605154410_AddLocalImagePathFieldToProductModelInProductAPIDb.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BimshireStore.Services.ProductAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLocalImagePathFieldToProductModelInProductAPIDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageLocalPath",
+                table: "Products",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 1,
+                column: "ImageLocalPath",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 2,
+                column: "ImageLocalPath",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 3,
+                column: "ImageLocalPath",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 4,
+                column: "ImageLocalPath",
+                value: null);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageLocalPath",
+                table: "Products");
+        }
+    }
+}
